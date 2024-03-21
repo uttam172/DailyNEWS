a9b33ba8ca6a3e606c23e5ad2b8ebbe09e34d6ef
uttam172 <uttamdobariya17@gmail.com>
2024-03-21 10:48:53 +0530

    8/4/2024

--- src/components/News.js
+++ src/components/News.js
@@ -25,12 +25,11 @@ export class News extends Component {
         }
     }
 
-    async componentDidMount() {
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=ec3ef80cf4054fee86e13e31f5f62a2b&page=1&pageSize=${this.props.pageSize}`;
+    async updateNews(){
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=ec3ef80cf4054fee86e13e31f5f62a2b&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
         let data = await fetch(url);
         let parsedData = await data.json();
-        console.log(parsedData);
         this.setState({
             articles: parsedData.articles,
             totalResults: parsedData.totalResults,
@@ -38,36 +37,20 @@ export class News extends Component {
         })
     }
 
+    async componentDidMount() {
+        this.updateNews();
+    }
+
     handlePreviousClick = async () => {
-        console.log("previous");
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&catagory=${this.props.category}&apiKey=ec3ef80cf4054fee86e13e31f5f62a2b&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-        this.setState({ loading: true });
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        console.log(parsedData);
-        this.setState({
-            page: this.state.page - 1,
-            articles: parsedData.articles,
-            loading: false
-        })
+        this.setState({page: this.state.page - 1});
+        this.updateNews();
     }
     handleNextClick = async () => {
-        console.log("next");
-        if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
-            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&catagory=${this.props.category}&apiKey=ec3ef80cf4054fee86e13e31f5f62a2b&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-            this.setState({ loading: true });
-            let data = await fetch(url);
-            let parsedData = await data.json();
-            this.setState({
-                page: this.state.page + 1,
-                articles: parsedData.articles,
-                loading: false
-            })
-        }
+        this.setState({page: this.state.page + 1});
+        this.updateNews();
     }
 
     render() {
-        console.log("render")
         return (
             <div className='container my-4'>
                 <h1 className="text-center">DailyNEWS - Top Headlines</h1>
@@ -75,7 +58,7 @@ export class News extends Component {
                 <div className="row">
                     {!this.state.loading && this.state.articles.map((element) => {
                         return <div className="col-md-4 my-4" key={element.url}>
-                            <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} />
+                            <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
                         </div>
                     })}
                 </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
-    
+
     render() {
-        let { title, description, imageUrl, newsUrl } = this.props;
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
         return (
             <div className="my-3">
-                <div className="card" style={{ width: "18rem" }}>
-                    <img src={!imageUrl? "https://images.macrumors.com/t/OpivTNFk5tiPViS0crXqhRqIQt8=/1607x/article-new/2024/02/macbook-air-blue-image.jpg" :imageUrl} className="card-img-top" alt="..." />
+                <div className="card" style={{ width: "auto", border: "none" }}>
+                    <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style={{marginLeft:'-10%', zIndex:'1'}}>{source}</span>
+
+                    <img src={!imageUrl ? "https://ichef.bbci.co.uk/news/1024/branded_news/15999/production/_132937488_7f178422942a84ceaf60990e6918a02af0e77eae0_0_3262_47131384x2000.jpg" : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-body-secondary">By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
                         <a rel="noreferrer" href={newsUrl} target='_blank' className="btn btn-sm btn-dark">Read more...</a>
                     </div>
                 </div>
